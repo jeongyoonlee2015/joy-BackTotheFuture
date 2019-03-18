f6fdae8eab7f8077f1626a2bdfd6539e29412392
JeongyoonLee <43804152+jeongyoonlee2015@users.noreply.github.com>
2019-03-18 12:38:52 +0900

    Add files via upload

---
+++ JavaScript/lecture10.js
@@ -0,0 +1,13 @@
+function return_test(){
+	return; // Quit
+	console.log("실행되지 않는다.");
+}
+function print(message){
+	console.log("print function in");
+	console.log(message);
+	console.log("print function out");
+}
+function sum(arg1, arg2){
+	var result + arg1 + arg2;
+	return result;
+}
