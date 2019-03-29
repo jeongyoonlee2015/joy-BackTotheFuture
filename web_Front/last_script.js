0bdd2de7ec412ca5593dc87a666100540e76e56c
JeongyoonLee <43804152+jeongyoonlee2015@users.noreply.github.com>
2019-03-30 08:43:36 +0900

    Update last_script.js

--- web_Front/last_script.js
+++ web_Front/last_script.js
@@ -1,5 +1,5 @@
 var req = new XMLHttpRequest();
-req.open("GET", "./image_list.json");
+req.open("GET", "./last_image_list.json");
 req.onreadystatechange = function(){
 	if(this.readyState == 4){
 		//console.log(this.response); 
@@ -18,6 +18,22 @@ req.onreadystatechange = function(){
 				}*/
 				this.classList.toggle("image-selected");//this=onclick
 				
+			}
+			div.onmouseover = function(){
+				//this=divElement
+
+				var element = this;
+				//이렇게 해주면 해결
+				this.timerId = setTimeout(function(){
+					//timerId를 this로 준다.
+					//this=해당 엘리먼트가 아님, 따라서 함수의 scope를 생각해야함
+
+					classList.add("image-magnified");
+				}, 1000);
+			}
+			div.onmouseout = function(){
+				clearTimeout(this.timerId);
+				this.classList.remove("image-magnified");
 			}
 			var img = document.createElement("img");
 			img.src = data[i];
@@ -42,4 +58,21 @@ function selectAll(btn){
 	}else{
 		btn.value = "Unselect All"
 	}
+}
+
+function slideShow(btn){
+	var images = document.getElementsByClassName("image");
+	var index = 0;
+	images[0].classList.add("image-magnified");
+	
+	var intervalId = setInterval(function(){
+		images[index].classList.remove("image-magnified");
+		index++;
+		if (index < images.length) {
+			images[index].classList.add("image-magnified");	
+		}
+		else{
+			clearInterval(intervalId);
+		}
+	}, 1000);
 }
